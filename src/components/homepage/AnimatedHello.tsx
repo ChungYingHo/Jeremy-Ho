27392ae81250feafefd3e6993f0379db614307c7
Jeremy <ag.cyho@gmail.com>
2025-04-20 20:59:52 +0800

    feat: add intro and btns in main page

--- src/components/homepage/AnimatedHello.tsx
+++ src/components/homepage/AnimatedHello.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { animate } from 'animejs';
 import styles from './index.module.css';
+import { useHistory } from '@docusaurus/router';
 
 export default function AnimatedHello() {
   const text = 'HELLO WORLD!';
+  const history = useHistory();
 
   useEffect(() => {
     animate(`.${styles.char}`, {
@@ -23,13 +25,26 @@ export default function AnimatedHello() {
   }, []);
 
   return (
-    <h2 className={styles.helloWrapper}>
-      {text.split('').map((char, idx) => (
-        <span key={idx} className={styles.letter}>
-          <span className={styles.ghost}>{char === ' ' ? '\u00A0' : char}</span>
-          <span className={styles.char}>{char === ' ' ? '\u00A0' : char}</span>
-        </span>
-      ))}
-    </h2>
+    <div className={styles.wrapper}>
+      <h2 className={styles.helloWrapper}>
+        {text.split('').map((char, idx) => (
+          <span key={idx} className={styles.letter}>
+            <span className={styles.ghost}>{char === ' ' ? '\u00A0' : char}</span>
+            <span className={styles.char}>{char === ' ' ? '\u00A0' : char}</span>
+          </span>
+        ))}
+      </h2>
+
+      <div className={styles.intro}>
+        <p className={styles.name}>Jeremy Ho</p>
+        <p className={styles.subtitle}>前端工程師 aka 遊戲開發設計師</p>
+      </div>
+
+      <div className={styles.buttons}>
+        <button onClick={() => history.push('/docs/notes')}>筆記</button>
+        <button onClick={() => history.push('/docs/series')}>系列</button>
+        <button onClick={() => history.push('/blog')}>Blog</button>
+      </div>
+    </div>
   );
 }
