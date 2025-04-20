5a95fcc590fc51ebc9fcda68831443e23b546edf
Jeremy <ag.cyho@gmail.com>
2025-04-20 14:17:39 +0800

    chore: setup mermaid and search plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,6 @@
 import type {Config} from '@docusaurus/types'
-import presets from './config/preset'
-import themeConfig from './config/theme'
+import presets from './config/presets'
+import themeConfig from './config/themeConfig'
 import plugins from './config/plugins'
 
 const config: Config = {
@@ -26,7 +26,24 @@ const config: Config = {
   // Preset features, theme UI and plugins settings
   presets,
   themeConfig,
-  plugins
+  plugins,
+
+  // setup mermaid and search plugin
+  markdown: {
+    mermaid: true,
+  },
+  themes: [
+    "@docusaurus/theme-mermaid",
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        hashed: true,
+        language: ["en", "zh"],
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+      },
+    ],
+  ],
 }
 
 export default config
