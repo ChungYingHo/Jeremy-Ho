e2fe43cca56cc70091dbdf1ec99ec9b0a10fa481
Jeremy <ag.cyho@gmail.com>
2025-04-20 19:37:13 +0800

    feat: move blog articles to new website

--- config/presets.ts
+++ config/presets.ts
@@ -19,6 +19,9 @@ const presets: PresetConfig[] = [
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
+          blogSidebarTitle: "最新文章",
+          blogSidebarCount: 10,
+          blogDescription: "非程式相關的日常生活雜談"
         },
         theme: {
           customCss: './src/css/custom.scss',
