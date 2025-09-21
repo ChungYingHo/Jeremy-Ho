5b279fbb9554e8a62b5f659980587cbf9c75caf2
Jeremy <ag.cyho@gmail.com>
2025-09-21 14:07:03 +0800

    doc: add java setup & type intro docs

---
+++ JavaBasicsExample.java
@@ -0,0 +1,9 @@
+public class JavaBasicsExample {
+
+    // 屬性
+    static String name = "Jeremy Ho";
+
+    public static void main(String[] args) {
+        System.out.println("Hello, " + name);
+    }
+}
